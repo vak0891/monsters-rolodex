8834beab464ae9c27283b062bc2a25fd654c412a
vakjc <vaishnavi@jumpcut.com>
2019-11-09 14:36:24 +0400

    Updates

---
+++ precache-manifest.ee2a89076b2077f5a02f52322dae99b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9810b840728ec1bc9dd7a34a7e1a61d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52e738e1c59af72bfb23",
+    "url": "/monsters-rolodex/static/css/main.dd5b120b.chunk.css"
+  },
+  {
+    "revision": "8f66e7892f88ac220728",
+    "url": "/monsters-rolodex/static/js/2.e8d38444.chunk.js"
+  },
+  {
+    "revision": "52e738e1c59af72bfb23",
+    "url": "/monsters-rolodex/static/js/main.72671dac.chunk.js"
+  },
+  {
+    "revision": "7f410422c000bddbff9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.a7e40423.js"
+  }
+]);
